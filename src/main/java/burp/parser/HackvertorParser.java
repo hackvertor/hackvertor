28334d6147ce60ede1b97e7f3e1d5cc8dd0bd6eb
Corey-Dean Arthur <1339555+CoreyD97@users.noreply.github.com>
2020-12-22 10:24:49 +0000

    Cleaner parser, better error handling

--- src/main/java/burp/parser/HackvertorParser.java
+++ src/main/java/burp/parser/HackvertorParser.java
@@ -47,6 +47,8 @@ LinkedList<Element> s = new LinkedList<Element>();
         case TEXT:
         case LESSTHAN:
         case ST_ERR:
+        case SELF_CLOSE_TAG_END:
+        case TAG_END:
         case IT_ERR:
         case QUOTED_STRING_VAL:
         case LITERAL_VAL:
@@ -73,9 +75,9 @@ s.addAll(e);
 
   final public LinkedList<Element> ElementSequence() throws ParseException {    try {
 LinkedList<Element> elements = new LinkedList<Element>();
- LinkedList<Element> nested = new LinkedList<Element>();
  Element e;
  Token text;
+ Token firstToken = getToken(1);
       try {
         switch ((jj_ntk==-1)?jj_ntk_f():jj_ntk) {
         case TAG_START:{
@@ -95,6 +97,8 @@ elements.add(new Element.TextElement("<")); {if ("" != null) return elements;}
           }
         case TEXT:
         case ST_ERR:
+        case SELF_CLOSE_TAG_END:
+        case TAG_END:
         case IT_ERR:
         case QUOTED_STRING_VAL:
         case LITERAL_VAL:
@@ -134,6 +138,14 @@ elements.add(new Element.TextElement("<")); {if ("" != null) return elements;}
             text = jj_consume_token(ARGS_END);
             break;
             }
+          case TAG_END:{
+            text = jj_consume_token(TAG_END);
+            break;
+            }
+          case SELF_CLOSE_TAG_END:{
+            text = jj_consume_token(SELF_CLOSE_TAG_END);
+            break;
+            }
           default:
             jj_la1[1] = jj_gen;
             jj_consume_token(-1);
@@ -148,8 +160,11 @@ elements.add(new Element.TextElement(text.image)); {if ("" != null) return eleme
           throw new ParseException();
         }
       } catch (ParseException ex) {
-{if ("" != null) return elements;} //Catch Exception for EOF
-
+//Catch any unexpected inputs including EOF and try to recover
+        token_source.SwitchTo(DEFAULT);
+        elements.addFirst(new Element.TextElement(getTokenText(firstToken, getToken(0))));
+        elements.addAll(ElementSequence());
+        {if ("" != null) return elements;}
       }
     throw new Error("Missing return statement in function");
     } finally {
@@ -164,35 +179,11 @@ LinkedList<Element> elements = new LinkedList<Element>();
     Token identifier = null;
     String arg;
     Token firstToken = getToken(1);
-      t = jj_consume_token(TAG_START);
-      switch ((jj_ntk==-1)?jj_ntk_f():jj_ntk) {
-      case FUNCTION_NAME:{
+      try {
+        t = jj_consume_token(TAG_START);
         identifier = jj_consume_token(FUNCTION_NAME);
-        break;
-        }
-      case TAG_START:
-      case ENDTAG_START:
-      case TEXT:
-      case LESSTHAN:
-      case ST_ERR:
-      case IT_ERR:
-      case QUOTED_STRING_VAL:
-      case LITERAL_VAL:
-      case COMMA:
-      case ARGS_END:
-      case ARG_ERR:{
-        elements = ElementSequence();
-elements.addFirst(new Element.TextElement(getTokenText(firstToken, t))); {if ("" != null) return elements;}
-        break;
-        }
-      default:
-        jj_la1[3] = jj_gen;
-        jj_consume_token(-1);
-        throw new ParseException();
-      }
-      switch ((jj_ntk==-1)?jj_ntk_f():jj_ntk) {
-      case ARGS_START:{
-        try {
+        switch ((jj_ntk==-1)?jj_ntk_f():jj_ntk) {
+        case ARGS_START:{
           jj_consume_token(ARGS_START);
           switch ((jj_ntk==-1)?jj_ntk_f():jj_ntk) {
           case QUOTED_STRING_VAL:
@@ -207,7 +198,7 @@ args.add(arg);
                 break;
                 }
               default:
-                jj_la1[4] = jj_gen;
+                jj_la1[3] = jj_gen;
                 break label_2;
               }
               jj_consume_token(COMMA);
@@ -217,37 +208,39 @@ args.add(arg);
             break;
             }
           default:
-            jj_la1[5] = jj_gen;
+            jj_la1[4] = jj_gen;
             ;
           }
           jj_consume_token(ARGS_END);
-        } catch (ParseException e) {
-token_source.SwitchTo(DEFAULT);
-            elements.addFirst(new Element.TextElement(getTokenText(firstToken, getToken(0))));
-            elements.addAll(ElementSequence());
-            {if ("" != null) return elements;}
-        }
-        break;
-        }
-      default:
-        jj_la1[6] = jj_gen;
-        ;
-      }
-      switch ((jj_ntk==-1)?jj_ntk_f():jj_ntk) {
-      case TAG_END:{
-        jj_consume_token(TAG_END);
-elements.add(new Element.StartTag(identifier.image, args)); {if ("" != null) return elements;}
-        break;
-        }
-      case SELF_CLOSE_TAG_END:
-      case SELF_CLOSE_TAG_END_AT:{
-        switch ((jj_ntk==-1)?jj_ntk_f():jj_ntk) {
-        case SELF_CLOSE_TAG_END:{
-          jj_consume_token(SELF_CLOSE_TAG_END);
           break;
           }
+        default:
+          jj_la1[5] = jj_gen;
+          ;
+        }
+        switch ((jj_ntk==-1)?jj_ntk_f():jj_ntk) {
+        case TAG_END:{
+          jj_consume_token(TAG_END);
+elements.add(new Element.StartTag(identifier.image, args)); {if ("" != null) return elements;}
+          break;
+          }
+        case SELF_CLOSE_TAG_END:
         case SELF_CLOSE_TAG_END_AT:{
-          jj_consume_token(SELF_CLOSE_TAG_END_AT);
+          switch ((jj_ntk==-1)?jj_ntk_f():jj_ntk) {
+          case SELF_CLOSE_TAG_END:{
+            jj_consume_token(SELF_CLOSE_TAG_END);
+            break;
+            }
+          case SELF_CLOSE_TAG_END_AT:{
+            jj_consume_token(SELF_CLOSE_TAG_END_AT);
+            break;
+            }
+          default:
+            jj_la1[6] = jj_gen;
+            jj_consume_token(-1);
+            throw new ParseException();
+          }
+elements.add(new Element.SelfClosingTag(identifier.image, args)); {if ("" != null) return elements;}
           break;
           }
         default:
@@ -255,13 +248,16 @@ elements.add(new Element.StartTag(identifier.image, args)); {if ("" != null) ret
           jj_consume_token(-1);
           throw new ParseException();
         }
-elements.add(new Element.SelfClosingTag(identifier.image, args)); {if ("" != null) return elements;}
-        break;
-        }
-      default:
-        jj_la1[8] = jj_gen;
-elements.addFirst(new Element.TextElement(getTokenText(firstToken, getToken(0)))); elements.addAll(ElementSequence()); {if ("" != null) return elements;}
+      } catch (ParseException e) {
+//        System.out.println("Failed Start tag. Treating as text");
+        elements.addFirst(new Element.TextElement(getTokenText(firstToken, getToken(0))));
       }
+      if (jj_2_1(2)) {
+        rest = ElementSequence();
+      } else {
+        ;
+      }
+if(rest != null) elements.addAll(rest); {if ("" != null) return elements;}
     throw new Error("Missing return statement in function");
     } finally {
       trace_return("StartTag");
@@ -282,7 +278,7 @@ Token t;
         break;
         }
       default:
-        jj_la1[9] = jj_gen;
+        jj_la1[8] = jj_gen;
         jj_consume_token(-1);
         throw new ParseException();
       }
@@ -294,51 +290,144 @@ Token t;
 
   final public LinkedList<Element> CloseTag() throws ParseException {    try {
 LinkedList<Element> elements = new LinkedList<Element>();
+    LinkedList<Element> rest = null;
     Token t;
     Token firstToken = getToken(1);
-      t = jj_consume_token(ENDTAG_START);
-      switch ((jj_ntk==-1)?jj_ntk_f():jj_ntk) {
-      case FUNCTION_NAME:{
+      try {
+        jj_consume_token(ENDTAG_START);
         t = jj_consume_token(FUNCTION_NAME);
-        break;
-        }
-      case TAG_START:
-      case ENDTAG_START:
-      case TEXT:
-      case LESSTHAN:
-      case ST_ERR:
-      case IT_ERR:
-      case QUOTED_STRING_VAL:
-      case LITERAL_VAL:
-      case COMMA:
-      case ARGS_END:
-      case ARG_ERR:{
-        elements = ElementSequence();
-elements.addFirst(new Element.TextElement(getTokenText(firstToken, t))); {if ("" != null) return elements;}
-        break;
-        }
-      default:
-        jj_la1[10] = jj_gen;
-        jj_consume_token(-1);
-        throw new ParseException();
-      }
-      switch ((jj_ntk==-1)?jj_ntk_f():jj_ntk) {
-      case TAG_END:{
         jj_consume_token(TAG_END);
 elements.add(new Element.EndTag(t.image)); {if ("" != null) return elements;}
-        break;
-        }
-      default:
-        jj_la1[11] = jj_gen;
-elements.addFirst(new Element.TextElement(getTokenText(firstToken, getToken(0))));
-            elements.addAll(ElementSequence());
-            {if ("" != null) return elements;}
+      } catch (ParseException e) {
+//        System.out.println("Failed End tag. Treating as text");
+        elements.addFirst(new Element.TextElement(getTokenText(firstToken, getToken(0))));
       }
+      if (jj_2_2(2)) {
+        rest = ElementSequence();
+      } else {
+        ;
+      }
+if(rest != null) elements.addAll(rest); {if ("" != null) return elements;}
     throw new Error("Missing return statement in function");
     } finally {
       trace_return("CloseTag");
     }
 }
+
+  private boolean jj_2_1(int xla)
+ {
+    jj_la = xla; jj_lastpos = jj_scanpos = token;
+    try { return (!jj_3_1()); }
+    catch(LookaheadSuccess ls) { return true; }
+    finally { jj_save(0, xla); }
+  }
+
+  private boolean jj_2_2(int xla)
+ {
+    jj_la = xla; jj_lastpos = jj_scanpos = token;
+    try { return (!jj_3_2()); }
+    catch(LookaheadSuccess ls) { return true; }
+    finally { jj_save(1, xla); }
+  }
+
+  private boolean jj_3R_7()
+ {
+    Token xsp;
+    xsp = jj_scanpos;
+    if (jj_scan_token(10)) {
+    jj_scanpos = xsp;
+    if (jj_scan_token(13)) {
+    jj_scanpos = xsp;
+    if (jj_scan_token(18)) {
+    jj_scanpos = xsp;
+    if (jj_scan_token(23)) {
+    jj_scanpos = xsp;
+    if (jj_scan_token(19)) {
+    jj_scanpos = xsp;
+    if (jj_scan_token(20)) {
+    jj_scanpos = xsp;
+    if (jj_scan_token(21)) {
+    jj_scanpos = xsp;
+    if (jj_scan_token(22)) {
+    jj_scanpos = xsp;
+    if (jj_scan_token(17)) {
+    jj_scanpos = xsp;
+    if (jj_scan_token(15)) return true;
+    }
+    }
+    }
+    }
+    }
+    }
+    }
+    }
+    }
+    return false;
+  }
+
+  private boolean jj_3R_6()
+ {
+    if (jj_scan_token(LESSTHAN)) return true;
+    return false;
+  }
+
+  private boolean jj_3R_5()
+ {
+    if (jj_3R_9()) return true;
+    return false;
+  }
+
+  private boolean jj_3_2()
+ {
+    if (jj_3R_3()) return true;
+    return false;
+  }
+
+  private boolean jj_3R_8()
+ {
+    if (!jj_rescan) trace_call("StartTag(LOOKING AHEAD...)");
+    if (jj_scan_token(TAG_START)) { if (!jj_rescan) trace_return("StartTag(LOOKAHEAD FAILED)"); return true; }
+    if (jj_scan_token(FUNCTION_NAME)) { if (!jj_rescan) trace_return("StartTag(LOOKAHEAD FAILED)"); return true; }
+    { if (!jj_rescan) trace_return("StartTag(LOOKAHEAD SUCCEEDED)"); return false; }
+  }
+
+  private boolean jj_3_1()
+ {
+    if (jj_3R_3()) return true;
+    return false;
+  }
+
+  private boolean jj_3R_4()
+ {
+    if (jj_3R_8()) return true;
+    return false;
+  }
+
+  private boolean jj_3R_3()
+ {
+    if (!jj_rescan) trace_call("ElementSequence(LOOKING AHEAD...)");
+    Token xsp;
+    xsp = jj_scanpos;
+    if (jj_3R_4()) {
+    jj_scanpos = xsp;
+    if (jj_3R_5()) {
+    jj_scanpos = xsp;
+    if (jj_3R_6()) {
+    jj_scanpos = xsp;
+    if (jj_3R_7()) { if (!jj_rescan) trace_return("ElementSequence(LOOKAHEAD FAILED)"); return true; }
+    }
+    }
+    }
+    { if (!jj_rescan) trace_return("ElementSequence(LOOKAHEAD SUCCEEDED)"); return false; }
+  }
+
+  private boolean jj_3R_9()
+ {
+    if (!jj_rescan) trace_call("CloseTag(LOOKING AHEAD...)");
+    if (jj_scan_token(ENDTAG_START)) { if (!jj_rescan) trace_return("CloseTag(LOOKAHEAD FAILED)"); return true; }
+    if (jj_scan_token(FUNCTION_NAME)) { if (!jj_rescan) trace_return("CloseTag(LOOKAHEAD FAILED)"); return true; }
+    { if (!jj_rescan) trace_return("CloseTag(LOOKAHEAD SUCCEEDED)"); return false; }
+  }
 
   /** Generated Token Manager. */
   public HackvertorParserTokenManager token_source;
@@ -348,15 +437,20 @@ elements.addFirst(new Element.TextElement(getTokenText(firstToken, getToken(0)))
   /** Next token. */
   public Token jj_nt;
   private int jj_ntk;
+  private Token jj_scanpos, jj_lastpos;
+  private int jj_la;
   private int jj_gen;
-  final private int[] jj_la1 = new int[12];
+  final private int[] jj_la1 = new int[9];
   static private int[] jj_la1_0;
   static {
 	   jj_la1_init_0();
 	}
 	private static void jj_la1_init_0() {
-	   jj_la1_0 = new int[] {0xfc2f00,0xfc2400,0xfc2f00,0xfc3f00,0x200000,0x180000,0x4000,0x18000,0x38000,0x180000,0xfc3f00,0x20000,};
+	   jj_la1_0 = new int[] {0xfeaf00,0xfea400,0xfeaf00,0x200000,0x180000,0x4000,0x18000,0x38000,0x180000,};
 	}
+  final private JJCalls[] jj_2_rtns = new JJCalls[2];
+  private boolean jj_rescan = false;
+  private int jj_gc = 0;
 
   {
       enable_tracing();
@@ -372,7 +466,8 @@ elements.addFirst(new Element.TextElement(getTokenText(firstToken, getToken(0)))
 	 token = new Token();
 	 jj_ntk = -1;
 	 jj_gen = 0;
-	 for (int i = 0; i < 12; i++) jj_la1[i] = -1;
+	 for (int i = 0; i < 9; i++) jj_la1[i] = -1;
+	 for (int i = 0; i < jj_2_rtns.length; i++) jj_2_rtns[i] = new JJCalls();
   }
 
   /** Reinitialise. */
@@ -386,7 +481,8 @@ elements.addFirst(new Element.TextElement(getTokenText(firstToken, getToken(0)))
 	 token = new Token();
 	 jj_ntk = -1;
 	 jj_gen = 0;
-	 for (int i = 0; i < 12; i++) jj_la1[i] = -1;
+	 for (int i = 0; i < 9; i++) jj_la1[i] = -1;
+	 for (int i = 0; i < jj_2_rtns.length; i++) jj_2_rtns[i] = new JJCalls();
   }
 
   /** Constructor. */
@@ -396,7 +492,8 @@ elements.addFirst(new Element.TextElement(getTokenText(firstToken, getToken(0)))
 	 token = new Token();
 	 jj_ntk = -1;
 	 jj_gen = 0;
-	 for (int i = 0; i < 12; i++) jj_la1[i] = -1;
+	 for (int i = 0; i < 9; i++) jj_la1[i] = -1;
+	 for (int i = 0; i < jj_2_rtns.length; i++) jj_2_rtns[i] = new JJCalls();
   }
 
   /** Reinitialise. */
@@ -414,7 +511,8 @@ elements.addFirst(new Element.TextElement(getTokenText(firstToken, getToken(0)))
 	 token = new Token();
 	 jj_ntk = -1;
 	 jj_gen = 0;
-	 for (int i = 0; i < 12; i++) jj_la1[i] = -1;
+	 for (int i = 0; i < 9; i++) jj_la1[i] = -1;
+	 for (int i = 0; i < jj_2_rtns.length; i++) jj_2_rtns[i] = new JJCalls();
   }
 
   /** Constructor with generated Token Manager. */
@@ -423,7 +521,8 @@ elements.addFirst(new Element.TextElement(getTokenText(firstToken, getToken(0)))
 	 token = new Token();
 	 jj_ntk = -1;
 	 jj_gen = 0;
-	 for (int i = 0; i < 12; i++) jj_la1[i] = -1;
+	 for (int i = 0; i < 9; i++) jj_la1[i] = -1;
+	 for (int i = 0; i < jj_2_rtns.length; i++) jj_2_rtns[i] = new JJCalls();
   }
 
   /** Reinitialise. */
@@ -432,7 +531,8 @@ elements.addFirst(new Element.TextElement(getTokenText(firstToken, getToken(0)))
 	 token = new Token();
 	 jj_ntk = -1;
 	 jj_gen = 0;
-	 for (int i = 0; i < 12; i++) jj_la1[i] = -1;
+	 for (int i = 0; i < 9; i++) jj_la1[i] = -1;
+	 for (int i = 0; i < jj_2_rtns.length; i++) jj_2_rtns[i] = new JJCalls();
   }
 
   private Token jj_consume_token(int kind) throws ParseException {
@@ -442,12 +542,48 @@ elements.addFirst(new Element.TextElement(getTokenText(firstToken, getToken(0)))
 	 jj_ntk = -1;
 	 if (token.kind == kind) {
 	   jj_gen++;
+	   if (++jj_gc > 100) {
+		 jj_gc = 0;
+		 for (int i = 0; i < jj_2_rtns.length; i++) {
+		   JJCalls c = jj_2_rtns[i];
+		   while (c != null) {
+			 if (c.gen < jj_gen) c.first = null;
+			 c = c.next;
+		   }
+		 }
+	   }
 	   trace_token(token, "");
 	   return token;
 	 }
 	 token = oldToken;
 	 jj_kind = kind;
 	 throw generateParseException();
+  }
+
+  @SuppressWarnings("serial")
+  static private final class LookaheadSuccess extends java.lang.Error { }
+  final private LookaheadSuccess jj_ls = new LookaheadSuccess();
+  private boolean jj_scan_token(int kind) {
+	 if (jj_scanpos == jj_lastpos) {
+	   jj_la--;
+	   if (jj_scanpos.next == null) {
+		 jj_lastpos = jj_scanpos = jj_scanpos.next = token_source.getNextToken();
+	   } else {
+		 jj_lastpos = jj_scanpos = jj_scanpos.next;
+	   }
+	 } else {
+	   jj_scanpos = jj_scanpos.next;
+	 }
+	 if (jj_rescan) {
+	   int i = 0; Token tok = token;
+	   while (tok != null && tok != jj_scanpos) { i++; tok = tok.next; }
+	   if (tok != null) jj_add_error_token(kind, i);
+	 } else {
+	   trace_scan(jj_scanpos, kind);
+	 }
+	 if (jj_scanpos.kind != kind) return true;
+	 if (jj_la == 0 && jj_scanpos == jj_lastpos) throw jj_ls;
+	 return false;
   }
 
 
@@ -481,6 +617,46 @@ elements.addFirst(new Element.TextElement(getTokenText(firstToken, getToken(0)))
   private java.util.List<int[]> jj_expentries = new java.util.ArrayList<int[]>();
   private int[] jj_expentry;
   private int jj_kind = -1;
+  private int[] jj_lasttokens = new int[100];
+  private int jj_endpos;
+
+  private void jj_add_error_token(int kind, int pos) {
+	 if (pos >= 100) {
+		return;
+	 }
+
+	 if (pos == jj_endpos + 1) {
+	   jj_lasttokens[jj_endpos++] = kind;
+	 } else if (jj_endpos != 0) {
+	   jj_expentry = new int[jj_endpos];
+
+	   for (int i = 0; i < jj_endpos; i++) {
+		 jj_expentry[i] = jj_lasttokens[i];
+	   }
+
+	   for (int[] oldentry : jj_expentries) {
+		 if (oldentry.length == jj_expentry.length) {
+		   boolean isMatched = true;
+
+		   for (int i = 0; i < jj_expentry.length; i++) {
+			 if (oldentry[i] != jj_expentry[i]) {
+			   isMatched = false;
+			   break;
+			 }
+
+		   }
+		   if (isMatched) {
+			 jj_expentries.add(jj_expentry);
+			 break;
+		   }
+		 }
+	   }
+
+	   if (pos != 0) {
+		 jj_lasttokens[(jj_endpos = pos) - 1] = kind;
+	   }
+	 }
+  }
 
   /** Generate ParseException. */
   public ParseException generateParseException() {
@@ -490,7 +666,7 @@ elements.addFirst(new Element.TextElement(getTokenText(firstToken, getToken(0)))
 	   la1tokens[jj_kind] = true;
 	   jj_kind = -1;
 	 }
-	 for (int i = 0; i < 12; i++) {
+	 for (int i = 0; i < 9; i++) {
 	   if (jj_la1[i] == jj_gen) {
 		 for (int j = 0; j < 32; j++) {
 		   if ((jj_la1_0[i] & (1<<j)) != 0) {
@@ -506,6 +682,9 @@ elements.addFirst(new Element.TextElement(getTokenText(firstToken, getToken(0)))
 		 jj_expentries.add(jj_expentry);
 	   }
 	 }
+	 jj_endpos = 0;
+	 jj_rescan_token();
+	 jj_add_error_token(0, 0);
 	 int[][] exptokseq = new int[jj_expentries.size()][];
 	 for (int i = 0; i < jj_expentries.size(); i++) {
 	   exptokseq[i] = jj_expentries.get(i);
@@ -567,6 +746,47 @@ elements.addFirst(new Element.TextElement(getTokenText(firstToken, getToken(0)))
 	   }
 	   System.out.println(" at line " + t1.beginLine + " column " + t1.beginColumn + ">; Expected token: <" + tokenImage[t2] + ">");
 	 }
+  }
+
+  private void jj_rescan_token() {
+	 jj_rescan = true;
+	 for (int i = 0; i < 2; i++) {
+	   try {
+		 JJCalls p = jj_2_rtns[i];
+
+		 do {
+		   if (p.gen > jj_gen) {
+			 jj_la = p.arg; jj_lastpos = jj_scanpos = p.first;
+			 switch (i) {
+			   case 0: jj_3_1(); break;
+			   case 1: jj_3_2(); break;
+			 }
+		   }
+		   p = p.next;
+		 } while (p != null);
+
+		 } catch(LookaheadSuccess ls) { }
+	 }
+	 jj_rescan = false;
+  }
+
+  private void jj_save(int index, int xla) {
+	 JJCalls p = jj_2_rtns[index];
+	 while (p.gen > jj_gen) {
+	   if (p.next == null) { p = p.next = new JJCalls(); break; }
+	   p = p.next;
+	 }
+
+	 p.gen = jj_gen + xla - jj_la; 
+	 p.first = token;
+	 p.arg = xla;
+  }
+
+  static final class JJCalls {
+	 int gen;
+	 Token first;
+	 int arg;
+	 JJCalls next;
   }
 
 }
