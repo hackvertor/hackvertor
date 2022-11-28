8bff4ab2d56b4dbeae53c78586cddc59d1e28069
Gareth Heyes <gareth.heyes@portswigger.net>
2022-11-28 11:25:43 +0000

    Added more random tags and fixed JavaScript execution to work on older versions of JVM

--- src/main/java/burp/BurpExtender.java
+++ src/main/java/burp/BurpExtender.java
@@ -183,7 +183,7 @@ public class BurpExtender implements IBurpExtender, ITab, IContextMenuFactory, I
                 }
                 try {
                     hackvertor = new Hackvertor();
-	            	stdout.println("Hackvertor v1.7.1");
+	            	stdout.println("Hackvertor v1.7.2");
                     loadCustomTags();
                     loadGlobalVariables();
                     registerPayloadProcessors();
