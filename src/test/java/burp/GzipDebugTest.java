e5599076cf8f5e5d686cc7a1c5c8fae973d75852
Gareth Heyes <gazheyes@gmail.com>
2025-07-18 09:00:12 +0100

    Fixed gzip test

--- src/test/java/burp/GzipDebugTest.java
+++ src/test/java/burp/GzipDebugTest.java
@@ -10,10 +10,6 @@ public class GzipDebugTest extends BaseHackvertorTest {
     void testSimpleGzipCompress() {
         String input = "test";
         String compressed = hackvertor.convert("<@gzip_compress>" + input + "</@gzip_compress>", hackvertor);
-        System.out.println("Input: " + input);
-        System.out.println("Compressed: " + compressed);
-        System.out.println("Compressed length: " + (compressed != null ? compressed.length() : "null"));
-        
         assertNotNull(compressed, "Compression should not return null");
         assertFalse(compressed.startsWith("Error:"), "Should not have error: " + compressed);
     }
@@ -21,18 +17,8 @@ public class GzipDebugTest extends BaseHackvertorTest {
     @Test
     void testGzipRoundTrip() {
         String input = "Hello World!";
-        System.out.println("Original: " + input);
-        
         String compressed = hackvertor.convert("<@gzip_compress>" + input + "</@gzip_compress>", hackvertor);
-        System.out.println("Compressed: " + compressed);
-        System.out.println("Compressed bytes: " + java.util.Arrays.toString(compressed.getBytes(java.nio.charset.StandardCharsets.ISO_8859_1)));
-        
         String decompressed = hackvertor.convert("<@gzip_decompress>" + compressed + "</@gzip_decompress>", hackvertor);
-        System.out.println("Decompressed: " + decompressed);
-        System.out.println("Decompressed equals original: " + input.equals(decompressed));
-        
-        if (decompressed.startsWith("Error:")) {
-            System.out.println("Decompression error: " + decompressed);
-        }
+        assertEquals(input, decompressed);
     }
 }
