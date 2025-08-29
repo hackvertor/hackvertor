84bbb2605ce55bb194a56628e197f4da56633a29
Gareth Heyes <gazheyes@gmail.com>
2025-08-29 14:22:37 +0100

    Testing github action

--- src/main/java/burp/hv/HackvertorExtension.java
+++ src/main/java/burp/hv/HackvertorExtension.java
@@ -33,7 +33,7 @@ import static burp.hv.HackvertorExtension.montoyaApi;
 public class HackvertorExtension implements BurpExtension, IBurpExtender, ITab, IExtensionStateListener, IMessageEditorTabFactory {
     //TODO Unset on unload
     public static String extensionName = "Hackvertor";
-    public static String version = "v2.0.22";
+    public static String version = "v2.0.23";
     public static JFrame HackvertorFrame = null;
     public static IBurpExtenderCallbacks callbacks;
     public static IExtensionHelpers helpers;
