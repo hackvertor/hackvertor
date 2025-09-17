29a06b6d9c8215fdc786e3a17a8a1d7df3ec2852
Gareth Heyes <gazheyes@gmail.com>
2025-09-17 13:58:41 +0100

    Fixed bug when importing rules.

--- src/main/java/burp/hv/tags/TagAutomator.java
+++ src/main/java/burp/hv/tags/TagAutomator.java
@@ -184,6 +184,7 @@ public class TagAutomator {
                                     enabled ? "✓" : "✗",
                                     rule.getString("name"),
                                     rule.getString("analysis"),
+                                    rule.getString("modification"),
                                     contexts
                                 });
                             }
